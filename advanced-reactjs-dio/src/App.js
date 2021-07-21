ef784690e959ba628cb645d65092e71652d7ac6a
thiagoviks <thiagosudgomes@gmail.com>
2021-07-21 14:56:29 -0300

    learning about reactjs hooks and lifecycle

--- advanced-reactjs-dio/src/App.js
+++ advanced-reactjs-dio/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Twitter from './Twitter';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    loading: false,
+  };
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        loading: true,
+      });
+    }, 3000);
+  }
+  render() {
+    const posts = [
+      {
+        title: 'xpto',
+        description: 'foo',
+      },
+      {
+        title: 'xpto2',
+        description: 'bar',
+      },
+    ];
+    return <Twitter posts={posts} loading={this.state.loading} />;
+  }
 }
 
 export default App;
---
+++ advanced-reactjs-dio/src/Twitter.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class Twitter extends Component {
+  /*
+    Learning about Hooks, more info on 
+    https://reactjs.org/docs/hooks-intro.html
+  */
+
+  componentDidMount() {
+    const { posts, loading } = this.props;
+    console.log('componentDidMount', posts);
+    console.log('componentDidMount:loading', loading);
+  }
+
+  componentDidUpdate(prevProps) {
+    const { loading } = this.props;
+    if (this.props.loading !== prevProps.loading) {
+      console.log('componentDidUpdate:loading', loading);
+    }
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount were removed');
+  }
+
+  render() {
+    const { posts } = this.props;
+    console.log('render', posts);
+    return <div>{posts.title}</div>;
+  }
+}
+
+export default Twitter;
