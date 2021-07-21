38550ba8402ae335c1a81e1701ac90b7aad56bd8
thiagoviks <thiagosudgomes@gmail.com>
2021-07-21 18:19:57 -0300

    implemented Hook useEffect

--- advanced-reactjs-dio/src/Twitter.js
+++ advanced-reactjs-dio/src/Twitter.js
@@ -1,51 +1,64 @@
-import React, { Component } from 'react';
-
-class Twitter extends Component {
-  /*
+/*
     Learning about Hooks, more info on 
     https://reactjs.org/docs/hooks-intro.html
   */
 
-  state = {
-    tweet: 'title',
-  };
-  componentDidMount() {
-    const { posts, loading } = this.props;
+import React, { useEffect, useState, memo } from 'react';
+
+const areEqual = (prevProps, nextProps) => {
+  return prevProps.loading === nextProps.loading;
+};
+
+function Twitter(props) {
+  const { loading } = props;
+  const [tweet, setTweet] = useState('title');
+  //componentDidMount
+  useEffect(() => {
+    const { posts, loading } = props;
     console.log('componentDidMount', posts);
     console.log('componentDidMount:loading', loading);
-  }
+  }, []);
 
-  componentDidUpdate(prevProps) {
-    const { loading } = this.props;
-    if (this.props.loading !== prevProps.loading) {
+  // componentDidUpdate
+  useEffect(() => {
+    console.log('componentDidUpdate');
+  }, [loading]);
+
+  // componentWillUnmount
+  useEffect(() => {
+    return () => {
+      console.log('componentWillUnmount were removed');
+    };
+  }, []);
+  /* 
+    componentDidUpdate = (prevProps) => {
+    const { loading } = props;
+    if (props.loading !== prevProps.loading) {
       console.log('componentDidUpdate:loading', loading);
     }
-  }
-
-  componentWillUnmount() {
-    console.log('componentWillUnmount were removed');
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    return this.state.tweet !== nextState.tweet;
-  }
-
-  tweet = () => {
-    this.setState({
-      tweet: true,
-    });
   };
-  render() {
-    const { posts } = this.props;
-    console.log('render', posts);
-    return (
-      <div>
-        <button onClick={this.tweet}>Re-render</button>
-        <br />
-        tests
-      </div>
-    );
-  }
+
+  componentWillUnmount = () => {
+    console.log('componentWillUnmount were removed');
+  };
+
+  shouldComponentUpdate = (nextProps, nextState) => {
+    return state.tweet !== nextState.tweet;
+  }; */
+
+  const handleTweet = () => {
+    setTweet('Tweet updated');
+  };
+
+  console.log('Tweet Updated: ', tweet);
+
+  return (
+    <div>
+      <button onClick={handleTweet}>Re-render</button>
+      <br />
+      tests
+    </div>
+  );
 }
 
-export default Twitter;
+export default memo(Twitter, areEqual);
